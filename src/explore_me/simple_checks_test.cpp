fa092441d99d2aa48a44894feec66d2c820b075a
Khaled Yakdan <yakdan@code-intelligence.com>
2023-12-17 16:20:39 +0100

    Add fuzz tests for the explore_me example

--- src/explore_me/simple_checks_test.cpp
+++ src/explore_me/simple_checks_test.cpp
@@ -11,3 +11,12 @@ TEST(ExploreSimpleChecksTests, DeveloperTest) {
 TEST(ExploreSimpleChecksTests, MaintainerTest) {
   EXPECT_NO_THROW(ExploreSimpleChecks(20, -10, "Maintainer"));
 }
+
+FUZZ_TEST(const uint8_t *data, size_t size) {
+  FuzzedDataProvider fdp(data, size);
+  int a = fdp.ConsumeIntegral<int>();
+  int b = fdp.ConsumeIntegral<int>();
+  std::string c = fdp.ConsumeRemainingBytesAsString();
+
+  ExploreSimpleChecks(a, b, c);
+}
