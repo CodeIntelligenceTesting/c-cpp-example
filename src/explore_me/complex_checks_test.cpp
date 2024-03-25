1df88991a323f72afa224112c4d9e527f40c0d03
Khaled Yakdan <yakdan@code-intelligence.com>
2024-03-25 07:58:36 +0100

    Exclude unit tests from fuzzing builds
    Needed to exclude unrelated GTest source files and headers from the coverage report

--- src/explore_me/complex_checks_test.cpp
+++ src/explore_me/complex_checks_test.cpp
@@ -1,8 +1,10 @@
 #include <cifuzz/cifuzz.h>
 #include <fuzzer/FuzzedDataProvider.h>
-#include <gtest/gtest.h>
 
 #include "explore_me.h"
+
+#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+#include <gtest/gtest.h>
 
 TEST(ExploreComplexChecksTests, DeveloperTest) {
   EXPECT_NO_THROW(ExploreComplexChecks(0, 10, "Developer"));
@@ -11,6 +13,8 @@ TEST(ExploreComplexChecksTests, DeveloperTest) {
 TEST(ExploreComplexChecksTests, MaintainerTest) {
   EXPECT_NO_THROW(ExploreComplexChecks(20, -10, "Maintainer"));
 }
+
+#endif
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
   FuzzedDataProvider fdp(data, size);
