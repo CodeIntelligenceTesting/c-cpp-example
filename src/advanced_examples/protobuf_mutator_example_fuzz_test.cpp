e40d8d5fee8f4ad6a453c20b3616c5d220bc0bc5
PBetzler <phil.betzler@gmx.de>
2024-07-24 17:03:08 +0200

    fixed accential removed endif statement

--- src/advanced_examples/protobuf_mutator_example_fuzz_test.cpp
+++ src/advanced_examples/protobuf_mutator_example_fuzz_test.cpp
@@ -10,6 +10,19 @@
 #ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
 #include <gtest/gtest.h>
 
+TEST(ExploreStructuredInputChecks, DeveloperTest) {
+    InputStruct inputStruct = (InputStrut) {.a=0, .b= 10, .c="Developer"};
+    EXPECT_NO_THROW(ExploreStructuredInputChecks(inputStruct));
+}
+
+TEST(ExploreStructuredInputChecks, MaintainerTest) {
+    InputStruct inputStruct = (InputStruct) {.a=20, .b= -10, .c="Maintainer"};
+    EXPECT_NO_THROW(ExploreStructuredInputChecks(inputStruct));
+}
+
+#endif
+
+
 // The DEFINE_PROTO_FUZZER macro is used to define a fuzz test
 // that receives a protobuf message as input. In this example,
 // we created a protobuf message that corresponds to the input
