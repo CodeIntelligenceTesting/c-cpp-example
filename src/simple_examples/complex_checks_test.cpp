35ca31cb1a7903892f5f8be84053dd87e62a0e6e
PBetzler <phil.betzler@gmx.de>
2024-08-09 16:30:25 +0200

    WIP: added some changes necessary for debug macro

--- src/simple_examples/complex_checks_test.cpp
+++ src/simple_examples/complex_checks_test.cpp
@@ -2,8 +2,6 @@
 #include <fuzzer/FuzzedDataProvider.h>
 
 #include "explore_me.h"
-
-#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
 #include <gtest/gtest.h>
 
 TEST(ExploreComplexChecksTests, DeveloperTest) {
@@ -13,8 +11,6 @@ TEST(ExploreComplexChecksTests, DeveloperTest) {
 TEST(ExploreComplexChecksTests, MaintainerTest) {
   EXPECT_NO_THROW(ExploreComplexChecks(20, -10, "Maintainer"));
 }
-
-#endif
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
   FuzzedDataProvider fdp(data, size);
--- src/simple_examples/simple_checks_test.cpp
+++ src/simple_examples/simple_checks_test.cpp
@@ -2,9 +2,8 @@
 #include <fuzzer/FuzzedDataProvider.h>
 
 #include "explore_me.h"
-
-#ifndef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
 #include <gtest/gtest.h>
+
 
 TEST(ExploreSimpleChecksTests, DeveloperTest) {
   EXPECT_NO_THROW(ExploreSimpleChecks(0, 10, "Developer"));
@@ -13,8 +12,6 @@ TEST(ExploreSimpleChecksTests, DeveloperTest) {
 TEST(ExploreSimpleChecksTests, MaintainerTest) {
   EXPECT_NO_THROW(ExploreSimpleChecks(20, -10, "Maintainer"));
 }
-
-#endif
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
   FuzzedDataProvider fdp(data, size);
