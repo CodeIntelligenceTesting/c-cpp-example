10e9b0b2f0d453979b8046bd4d114e8d9d549b42
PBetzler <phil.betzler@gmx.de>
2024-08-14 09:46:46 +0200

    WIP: changed gtest build script

--- cmake/external/googletest.cmake
+++ cmake/external/googletest.cmake
@@ -9,35 +9,35 @@ set(GTEST_MAIN_LIBRARIES gtest_main)
 set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES})
 
 foreach(lib IN LISTS GTEST_BOTH_LIBRARIES)
-  if (MSVC)
-    if (CMAKE_BUILD_TYPE MATCHES Debug)
-      set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/${lib}d.lib)
+    if (MSVC)
+        if (CMAKE_BUILD_TYPE MATCHES Debug)
+            set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/${lib}d.lib)
+        else()
+            set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/${lib}.lib)
+        endif()
     else()
-      set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/${lib}.lib)
+        set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/lib${lib}.a)
     endif()
-  else()
-    set(LIB_PATH ${GTEST_INSTALL_DIR}/lib/lib${lib}.a)
-  endif()
-  list(APPEND GTEST_BUILD_BYPRODUCTS ${LIB_PATH})
+    list(APPEND GTEST_BUILD_BYPRODUCTS ${LIB_PATH})
 
-  add_library(${lib} STATIC IMPORTED)
-  set_property(TARGET ${lib} PROPERTY IMPORTED_LOCATION
-               ${LIB_PATH})
-  add_dependencies(${lib} ${GTEST_TARGET})
+    add_library(${lib} STATIC IMPORTED)
+    set_property(TARGET ${lib} PROPERTY IMPORTED_LOCATION
+            ${LIB_PATH})
+    add_dependencies(${lib} ${GTEST_TARGET})
 endforeach(lib)
 
 include (ExternalProject)
 ExternalProject_Add(${GTEST_TARGET}
-    PREFIX ${GTEST_TARGET}
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG v1.14.0
-    UPDATE_COMMAND ""
-    CMAKE_CACHE_ARGS -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-                     -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-                     -DCMAKE_C_COMPILER_LAUNCHER:FILEPATH=${CMAKE_C_COMPILER_LAUNCHER}
-                     -DCMAKE_CXX_COMPILER_LAUNCHER:FILEPATH=${CMAKE_CXX_COMPILER_LAUNCHER}
-    CMAKE_ARGS ${CMAKE_ARGS}
-               -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR}
-               -DCMAKE_INSTALL_LIBDIR=lib
-    BUILD_BYPRODUCTS ${GTEST_BUILD_BYPRODUCTS}
+        PREFIX ${GTEST_TARGET}
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.14.0
+        UPDATE_COMMAND ""
+        CMAKE_CACHE_ARGS -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER_LAUNCHER:FILEPATH=${CMAKE_C_COMPILER_LAUNCHER}
+        -DCMAKE_CXX_COMPILER_LAUNCHER:FILEPATH=${CMAKE_CXX_COMPILER_LAUNCHER}
+        CMAKE_ARGS ${CMAKE_ARGS}
+        -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL_DIR}
+        -DCMAKE_INSTALL_LIBDIR=lib
+        BUILD_BYPRODUCTS ${GTEST_BUILD_BYPRODUCTS}
 )
