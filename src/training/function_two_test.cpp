80269573ccd9d4d90b20412166384006d4b6e88e
PBetzler <phil.betzler@gmx.de>
2024-08-21 14:56:47 +0000

    completed second fuzzing exercise

--- src/training/function_two_test.cpp
+++ src/training/function_two_test.cpp
@@ -13,5 +13,12 @@ TEST(ExploreComplexChecksTests, MaintainerTest) {
 }
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
-    // TODO write fuzz test yourself!
+  // transforming fuzzing data into the format we need for testing the target function
+  FuzzedDataProvider fdp(data, size);
+  int a = fdp.ConsumeIntegral<int>();
+  int b = fdp.ConsumeIntegral<int>();
+  std::string c = fdp.ConsumeRemainingBytesAsString();
+
+  // calling the target function with the transformed fuzzing data
+  FunctionTwo(a,b, &c.front(), c.size());
 }
