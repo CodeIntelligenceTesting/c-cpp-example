e15be128d07db2b266828046d260651131cb2ca4
PBetzler <phil.betzler@gmx.de>
2024-09-12 18:00:22 +0200

    updated example to new naming schema

--- src/advanced_examples/slow_input_checks_test.cpp
+++ src/advanced_examples/slow_input_checks_test.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 
 TEST(ExploreSlowInputsChecks, FirstTest) {
-    
+
     EXPECT_NO_THROW(ExploreSlowInputsChecks(23323, 100));
 }
 
@@ -13,7 +13,7 @@ TEST(ExploreSlowInputsChecks, SecondTest) {
     EXPECT_NO_THROW(ExploreSlowInputsChecks(1324153, 192198));
 }
 
-DEBUG_FINDING(philosophical_capybara)
+DEBUG_FINDING(predictive_ant)
 FUZZ_TEST(const uint8_t *data, size_t size) {
     FuzzedDataProvider fdp(data, size);
     long a = fdp.ConsumeIntegral<int>();
